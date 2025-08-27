74620178fd50966ca22073ff45a9a001dc7eca4c
kevinlp9 <atilano.kevin.h17@gmail.com>
2025-08-27 11:40:21 -0600

    27/08/25

--- src/clases/Transaction.java
+++ src/clases/Transaction.java
@@ -1,16 +1,20 @@
 package clases;
 
+import java.time.LocalDateTime;
+
 public class Transaction {
     private static int contador;
 
     private final int id;
     private final String tipo;
     private final double monto;
+    private LocalDateTime fecha;
 
     public Transaction(String tipo, double monto) {
         this.id = ++contador;
         this.tipo = tipo;
         this.monto = monto;
+        this.fecha = LocalDateTime.now();
     }
 
     public int getId() {
@@ -25,12 +29,17 @@ public class Transaction {
         return monto;
     }
 
+    public LocalDateTime getFecha() {
+        return fecha;
+    }
+
     @Override
     public String toString() {
         return "Transaction{" +
                 "id=" + id +
-                ", tipo='" + tipo + '\'' +
-                ", monto=" + monto +
+                ", tipo:'" + tipo + '\'' +
+                ", monto:" + monto +
+                ", fecha: " + fecha +
                 '}';
     }
 }
